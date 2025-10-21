3eb436cc0f75658cb395df7deec51a231722821e
Nikhil Kumar <89528792+hynatshu@users.noreply.github.com>
2025-10-21 15:04:06 +0530

    Delete dest directory

--- dest/src/locales/index.js
+++
@@ -1,8 +0,0 @@
-import en from "./en.json" with { type: "json" };
-import vi from "./vi.json" with { type: "json" };
-import tr from "./tr.json" with { type: "json" };
-export default {
-    en,
-    vi,
-    tr,
-};
--- dest/src/typings/constants.js
+++
@@ -1,6 +0,0 @@
-export const NORMALIZE_REGEX = /[\p{Cf}\p{Cc}\p{Zl}\p{Zp}\p{Cn}]/gu;
-export const COLOR = {
-    CRITICAL: "#FF0000",
-    NORMAL: "#FFFF00",
-    LOW: "#00FF00",
-};
